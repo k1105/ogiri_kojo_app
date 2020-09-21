073b22e4371af9e628d4b06a28e8f37adaa03fc1
Ubuntu <ubuntu@ip-172-31-29-1.us-east-2.compute.internal>
2020-09-21 13:17:52 +0000

    display question on answer page

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -4,20 +4,23 @@ class AnswersController < ApplicationController
     if @answer.save
       redirect_to answer_path(answer_id: @answer.id)
     else
-      render 'question'
+      @question = answer_params[:question_id]
+      render 'questions/question'
     end
   end
   
   def answer #回答表示ページ。
-    if params[:answer_id].nil?#params[:answer_id]はcreateアクションを経由したときのみ与えられる
+    if params[:answer_id].nil? #params[:answer_id]はcreateアクションを経由したときのみ与えられる
       redirect_to root_path
     else
       @answer = Answer.find(params[:answer_id])
+      @best_answers = Answer.where(question_id: @answer.question_id)
+      @question = @answer.question
     end
   end
   
   private
     def answer_params
-      params.require(:answer).permit(:text)
+      params.require(:answer).permit(:text, :question_id)
     end
 end
